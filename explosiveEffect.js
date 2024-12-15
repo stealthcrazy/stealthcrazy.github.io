85cc536469e5d62e269397660824d0aec6833075
Sohan Prabhu <78947397+stealthcrazy@users.noreply.github.com>
2024-12-15 19:05:54 +0000

    Add files via upload

---
+++ explosiveEffect.js
@@ -0,0 +1,21 @@
+const enhance = id => {
+
+    const element = document.getElementById(id),
+          text = element.innerText.split("");
+    
+    element.innerText = '';
+
+    text.forEach( letter => {
+        const span = document.createElement("span");
+
+        span.className = 'letter';
+        span.innerText = letter;
+
+        element.appendChild(span);
+
+    });
+
+
+
+}
+enhance('explosive');
